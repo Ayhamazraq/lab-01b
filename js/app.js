66fc91aa8c6723c74511df200e9b513a4b8709ad
Ayhamazraq <ayham.bluee@gmail.com>
2021-08-01 19:30:15 +0300

    add new file

---
+++ js/app.js
@@ -0,0 +1,18 @@
+var namee = prompt("what is your name") 
+alert ("hey, wlecome to my website " + namee )
+console.log(namee);
+
+var donation = prompt("what you think about donation?, please give me your opnion")
+alert ("its really good  " + donation )
+console.log(donation);
+
+
+
+var catg = prompt("so you prefer make donation for children or studying cases or health cases")
+alert ("great opnion for " + catg )
+console.log(catg);
+
+var payment = prompt("wich way do you like for make donation ? cash or visa card")
+alert ("good way " + payment )
+console.log(payment)
+
